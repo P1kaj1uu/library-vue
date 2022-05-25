6f507d75f475316ac62185c617db8e9a46ec2bf5
P1kaj1uu <891523233@qq.com>
2022-05-25 14:58:49 +0800

    初始化

---
+++ src/views/Login/index.vue
@@ -0,0 +1,24 @@
+<template>
+ <div class="login_container">
+   <div class="login_box">22</div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login_box {
+  height: 100%;
+  background: url(../../assets/1.jpg);
+  background-color: red;
+}
+</style>
